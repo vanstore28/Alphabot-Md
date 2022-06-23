b0608a348ad40e499baaec78409a736d8f0b9e6c
vanstore28 <107975047+vanstore28@users.noreply.github.com>
2022-06-23 16:21:33 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Evan x Storeོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ van botzོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.evan.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UCxhSFuQy_HXnSU86SILmA9A" //bebas asal jan hapus
+global.botname = "Alphabot-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6281286743277" // nomor wa kalian
-global.ownernomerr = "+6281286743277" //nmr wa kalian
+global.ownernomer = "62887435047326" // nomor wa kalian
+global.ownernomerr = "+62887435047326" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "https://telegra.ph/file/04bab09a50597ebd5b956.jpg" // foto donasi di folder image
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6281286743277","6281286743277","918156874290"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© EvanBotzོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Evan x store' //sticker wm ganti nama kalian
+global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Alphabot-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
